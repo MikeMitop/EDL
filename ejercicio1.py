156c9879caef00846fa8e07cbc4f06db94b81c4b
Miguel Celis <164904971+MikeMitop@users.noreply.github.com>
2025-03-11 10:12:06 -0500

    Update ejercicio1.py

--- ejercicio1.py
+++ ejercicio1.py
@@ -30,16 +30,3 @@ cola = elim_dato(cola, 30)
 print("Cola luego de eliminar la primera ocurrencia del dato:")
 print(list(cola.queue))
 
-
-# Readme.md
-
-# Ejercicio 1
-### Enunciado
-# # 1. Eliminar elemento.
-# Escriba una función que reciba como parámetro una cola y un dato. La función debe:
-# 1. Eliminar el dato de la cola
-# 2. Mostrar por consola la cola con lados en el orden de llegada luego de eliminar el dato.
-# 3. La función no debe declarar una estructura de datos dinámica o estática auxiliar para
-# eliminar el dato de la cola.
-
-### Solución
